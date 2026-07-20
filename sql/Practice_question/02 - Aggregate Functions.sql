57160ab5dfec7a28df7e84ff1fc356f9bab806b2
Onkar Jadhav <onkar@example.com>
2026-07-20 17:16:23 +0530

    group by clause

--- sql/Practice_question/02 - Aggregate Functions.sql
+++ sql/Practice_question/02 - Aggregate Functions.sql
@@ -8,24 +8,43 @@
 
 -- Q1. Display the total number of customers.
 
+select count(*) from customers;
+
 -- Q2. Display the total number of products.
+
+select count(*) from products;
 
 -- Q3. Display the total revenue generated from all orders.
 -- (Hint: Use total_amount from the orders table.)
 
+select sum(total_amount) from orders;
+
 -- Q4. Display the average selling price of all products.
+
+select avg(selling_price) from products;
 
 -- Q5. Display the highest selling price among all products.
 
+select max(selling_price) from products;
+
 -- Q6. Display the lowest selling price among all products.
+
+select min(selling_price) from products;
 
 -- Q7. Display the total quantity of products sold.
 -- (Hint: Use the order_items table.)
 
+select sum(quantity) from order_items;
+
 -- Q8. Display the average product rating.
 -- (Hint: Use the reviews table.)
 
+select avg(rating) from reviews;
+
 -- Q9. Display the total number of unique cities where customers live.
 
+select distinct(city) from addresses;
+
 -- Q10. Display the total payment received.
--- (Hint: Use payment_amount from the payments table.)
+
+select sum(payment_amount) from payments where payment_status ='success';
